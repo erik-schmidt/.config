110129b98b7cfec01dbd539ea103693a18e0d366
Erik Schmidt <erik.schmidt@bcmsolutions.de>
2021-09-22 10:59:19 +0200

    Add alacritty config & colorscheme for neovim

--- nvim/lua/settings/colorscheme.lua
+++ nvim/lua/settings/colorscheme.lua
@@ -4,4 +4,5 @@ vim.g.everforest_background = 'soft'
 vim.g.everforest_diagnostic_virtual_text = 'colored'
 vim.g.everforest_diagnostic_line_highlight = 1
 vim.g.everforest_current_word = 'underline'
+vim.g.everforest_transparent_background = 1
 vim.cmd('colorscheme everforest')
